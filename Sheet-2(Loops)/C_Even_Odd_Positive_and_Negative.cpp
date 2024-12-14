90457ea57ff92f904065fd85a6954bfe41102d7c
S.M.Arifuzzaman <smarifuzzaman123@gmail.com>
2024-12-14 23:28:54 +0600

    solved

--- Sheet-2(Loops)/C_Even_Odd_Positive_and_Negative.cpp
+++ Sheet-2(Loops)/C_Even_Odd_Positive_and_Negative.cpp
@@ -6,19 +6,18 @@ int main()
 {
     int t;
     cin >> t;
-    int even, odd, pos, neg;
-    even = odd = pos = neg = 0;
+    int even = 0, odd = 0, pos = 0, neg = 0;
+
     while(t--){
         int n;
         cin >> n; 
-        
-        if(n % 2 == 0) even++;
-        if(n % 2 != 0 && n==0) odd++;
+        if(n % 2 == 0) even++; 
+        if(n % 2 != 0) odd++;
         if(n > 0) pos++;
         if(n < 0) neg++;
     }
     cout << "Even: " << even << endl;
-    cout << "Odd: " << even << endl;
+    cout << "Odd: " << odd << endl;
     cout << "Positive: " << pos << endl;
     cout << "Negative: " << neg << endl;
 
---
+++ Sheet-2(Loops)/D_Fixed_Password.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    while (1)
+    {
+        int pass;
+        cin >> pass;
+        if (pass == 1999)
+        {
+            cout << "Correct" << endl;
+            break;
+        }
+        else
+        {
+            cout << "Wrong" << endl;
+        }
+    }
+    return 0;
+}
