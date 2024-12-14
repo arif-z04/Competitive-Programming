70385db97d5deaa97df9122ce1e2eba83e6a04ff
S.M.Arifuzzaman <smarifuzzaman123@gmail.com>
2024-12-14 17:09:19 +0600

    new files

---
+++ Sheet-2(Loops)/B_Even_Numbers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        if(i%2==0){
+            cout << i << endl;
+        }
+    }
+
+    if(n < 1){
+        cout << "-1" << endl; 
+    }
+    return 0;
+}
